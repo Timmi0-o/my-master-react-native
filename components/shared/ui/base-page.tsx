a668756049e41329921eeb58ab48fe957d37a6a8
Timmy <tima53419@gmail.com>
2026-05-18 19:48:12 +0300

    fix: update API URL and refactor BasePage component for improved layout

--- components/shared/ui/base-page.tsx
+++ components/shared/ui/base-page.tsx
@@ -1,14 +1,8 @@
 import { useThemeApp } from '@/configs/theme/theme-context'
 import { THEME_BACKGROUND_COLORS } from '@/constants/theme-colors'
 import type { ReactElement, ReactNode } from 'react'
-import {
-	Platform,
-	ScrollView,
-	StyleSheet,
-	type StyleProp,
-	type ViewStyle,
-} from 'react-native'
-import { useSafeAreaInsets, type Edge } from 'react-native-safe-area-context'
+import { ScrollView, type StyleProp, type ViewStyle } from 'react-native'
+import { SafeAreaView, type Edge } from 'react-native-safe-area-context'
 
 interface IBasePageProps {
 	children: ReactNode
@@ -28,23 +22,22 @@ export function BasePage({
 }: IBasePageProps): ReactElement {
 	const { resolvedColorScheme } = useThemeApp()
 
-	const { bottom, top } = useSafeAreaInsets()
-
-	const os = Platform.OS
-
-	const styles = StyleSheet.create({
-		container: {
-			flex: 1,
-			backgroundColor: THEME_BACKGROUND_COLORS[resolvedColorScheme],
-			...(!disableTopSafeArea
-				? { [`${os === 'ios' ? 'marginTop' : 'paddingTop'}`]: top }
-				: {}),
-			[`${os === 'ios' ? 'marginBottom' : 'paddingBottom'}`]: bottom + 30,
-		},
-		scrollView: {
-			flex: 1,
-		},
-	})
-
-	return <ScrollView style={[styles.scrollView, style]}>{children}</ScrollView>
+	return (
+		<SafeAreaView
+			style={{
+				backgroundColor: THEME_BACKGROUND_COLORS[resolvedColorScheme],
+				flex: 1,
+			}}
+		>
+			<ScrollView
+				contentContainerStyle={{
+					backgroundColor: THEME_BACKGROUND_COLORS[resolvedColorScheme],
+					flex: 1,
+				}}
+				style={style}
+			>
+				{children}
+			</ScrollView>
+		</SafeAreaView>
+	)
 }
