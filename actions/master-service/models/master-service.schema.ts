d89327edafd0824bfc6fbc34b0f526e4e239d4af
Timmy <tima53419@gmail.com>
2026-06-24 17:23:55 +0300

    feat: update MasterServiceSchema (add MasterServiceImageFileSchema)

--- actions/master-service/models/master-service.schema.ts
+++ actions/master-service/models/master-service.schema.ts
@@ -1,6 +1,28 @@
 import { z } from 'zod'
 import { MasterProfileSchema } from '@/actions/master/models/master-profile.schema'
 
+const MasterServiceImageFileSchema = z.object({
+	id: z.string(),
+	fileUrl: z.string(),
+	originalName: z.string(),
+	mimeType: z.string(),
+	fileType: z.string(),
+	purpose: z.string(),
+	status: z.string(),
+	fileSize: z.number(),
+	createdAt: z.string().optional(),
+	updatedAt: z.string().optional(),
+})
+
+const MasterServiceImageSchema = z.object({
+	id: z.string(),
+	masterServiceId: z.string(),
+	fileId: z.string(),
+	createdAt: z.string().optional(),
+	updatedAt: z.string().optional(),
+	file: MasterServiceImageFileSchema.optional(),
+})
+
 export const MasterServiceSchema = z.object({
 	id: z.string(),
 	name: z.string(),
@@ -18,6 +40,7 @@ export const MasterServiceSchema = z.object({
 		description: true,
 		rating: true,
 	}).optional(),
+	images: z.array(MasterServiceImageSchema).optional(),
 })
 
 export const MasterServiceGetManyParamsSchema = z.object({
